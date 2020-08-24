87627ee248ee9d2f0afa9b6dff292268145124db
Marcus Davanço <md.daves@gmail.com>
2020-08-24 00:25:47 -0300

    feature: finished chapter 1 of Linguagem C - Luís Damas

---
+++ 01/prog0104.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+void main(){
+    printf("C\nis the greatest \nLanguage\n");
+}
---
+++ 01/prog0107.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void main(){
+    printf("Aqui vai um Apito \a\n");
+    printf("Aqui vai um Apito \7\n");
+    
+}
